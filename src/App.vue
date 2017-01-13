4b6c92d7ab4fd70c47483fb84655e6737058a5de
dagos <afrian.junior26@gmail.com>
2017-01-13 09:08:32 +0700

    add opening page

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,5 @@
 </script>
 
 <style>
-</style>
-
-<style>
-
+  
 </style>
--- src/modules/Home/Pages/Base.vue
+++ src/modules/Home/Pages/Base.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-
+    <div class="opening">
+      <div class="opening__box">
+        <img src="../../../assets/logo.png" alt="Vue2 SSR"/>
+        <div class="opening__title">Vue 2 Server Side Rendering</div>
+        <div class="opening__suggest">
+          <a href="https://github.com/afrianjunior" target="_blank">Github</a>
+          <a href="https://github.com/afrianjunior/CSS-kit" target="_blank">CSS Kit</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,11 +17,55 @@
   export default {
     data () {
       return {
-        input: ''
+        // data here
       }
-    }
+    },
+    mounted () {
+      // first of all
+    },
+    computed: {},
+    methods: {},
+    components: {}
   }
 </script>
 
 <style>
+  .opening {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .opening__box {
+    text-align: center;
+  }
+
+  .opening__box > img {
+    margin-bottom: 2em;
+  }
+
+  .opening__box > .opening__title {
+    text-align: center;
+    color: rgb(108, 108, 108);
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 100;
+    font-size: 40px;
+  }
+
+  .opening__box > .opening__suggest {
+    font-family: 'Open Sans', sans-serif;
+    margin: 1em 0;
+    font-size: 16px;
+  }
+
+  .opening__box > .opening__suggest a {
+    color: rgb(29, 180, 62);
+    text-decoration: none;
+    margin-right: 20px;
+  }
+
+  .opening__box > .opening__suggest a:last-child {
+    margin-right: 0;
+  }
 </style>
